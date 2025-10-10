760d1ac9e6e74c062ef4004ebc118447a84dc098
Diego Becerra <d.becerraz@uniandes.edu.co>
2025-10-09 22:55:44 -0500

    Feat: minimal libs setup

--- apps/inventario/src/app/app.module.ts
+++ apps/inventario/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MediSupplyConfigEnvModule } from '@medi-supply/config-env';
+import { MediSupplyDatabaseModule } from '@medi-supply/database';
+import { MediSupplySharedModule } from '@medi-supply/shared';
 
 @Module({
-  imports: [],
+  imports: [MediSupplyConfigEnvModule, MediSupplyDatabaseModule, MediSupplySharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
