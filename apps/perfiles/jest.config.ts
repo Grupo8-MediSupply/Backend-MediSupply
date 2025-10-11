67aefab66a8f53986b8143a666f0b18b03261c53
Diego Becerra <d.becerraz@uniandes.edu.co>
2025-10-11 18:00:41 -0500

    feat: develop workflow set

--- apps/perfiles/jest.config.ts
+++ apps/perfiles/jest.config.ts
@@ -20,7 +20,6 @@ export default {
   coverageDirectory: 'test-output/jest/coverage',
   coverageThreshold: {
     global: {
-      branches: 80,
       functions: 80,
       lines: 80,
       statements: 80,
