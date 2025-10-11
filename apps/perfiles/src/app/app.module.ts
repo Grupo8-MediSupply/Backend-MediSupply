d608fa7f6bd78ec1d53ab193ee6e3ea2f053360d
Mateo Caceres Arcila <m.caceresa23@uniandes.edu.co>
2025-10-10 22:40:18 -0500

    feat: crear proveedores

--- apps/perfiles/src/app/app.module.ts
+++ apps/perfiles/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MediSupplyDatabaseModule } from '@medi-supply/database';
 import { MediSupplySharedModule } from '@medi-supply/shared';
 import { MediSupplyPerfilesDmModule } from '@medi-supply/perfiles-dm';
 import { VendedoresModule } from './vendedores/vendedores.module';
+import { ProveedoresModule } from './proveedores/proveedores.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { VendedoresModule } from './vendedores/vendedores.module';
     MediSupplySharedModule,
     MediSupplyPerfilesDmModule,
     VendedoresModule,
+    ProveedoresModule,
   ],
   controllers: [AppController],
   providers: [AppService],
