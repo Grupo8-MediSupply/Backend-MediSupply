5e9722be42383dae8670fe632ada8f6a2bfa4c0b
Diego Becerra <d.becerraz@uniandes.edu.co>
2025-10-19 15:44:51 -0500

    feat: enable CORS and configure Fastify for multiple applications

--- apps/inventario/src/main.ts
+++ apps/inventario/src/main.ts
@@ -1,10 +1,11 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app/app.module';
+import fastifyCors from '@fastify/cors'; // 👈 Importa el plugin
 
 async function bootstrap() {
   // 🔹 Creamos la app con FastifyAdapter
@@ -13,6 +14,18 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
 
+  // 🌍 Habilitar CORS con Fastify
+  await app.register(fastifyCors, {
+    origin: [
+      'https://medisupply-474900.web.app', // frontend en Firebase
+      'https://medi-g8-b0kxqvrx.ue.gateway.dev', // API Gateway
+      'http://localhost:4200', // opcional para desarrollo local
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // si manejas cookies o headers auth
+  });
+
   // 🔹 Prefijo global
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
@@ -20,7 +33,7 @@ async function bootstrap() {
   // 🔹 Puerto configurable
   const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
-  // 🔹 Escuchar en 0.0.0.0 (útil para Docker)
+  // 🔹 Escuchar en 0.0.0.0 (útil para Cloud Run o Docker)
   await app.listen({ port, host: '0.0.0.0' });
 
   // 🔹 Logging
