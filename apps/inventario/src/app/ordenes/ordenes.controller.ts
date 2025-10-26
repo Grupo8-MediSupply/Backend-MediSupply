b92c3279dca92be8ccb68c59c78d6d6f06bd9ff8
Diego Becerra <d.becerraz@uniandes.edu.co>
2025-10-26 17:24:42 -0500

    feat(ordenes): update controller to versioned endpoint and clean up service method

--- apps/inventario/src/app/ordenes/ordenes.controller.ts
+++ apps/inventario/src/app/ordenes/ordenes.controller.ts
@@ -3,12 +3,12 @@ import { CrearOrdenClienteDto } from './dtos/crear-orden.dto';
 import { OrdenesService } from './ordenes.service';
 import { User, type JwtPayloadDto } from '@medi-supply/shared';
 
-@Controller('ordenes')
+@Controller('v1/ordenes')
 export class OrdenesController {
     constructor(private readonly ordenesService: OrdenesService) {}
 
-    @Post('/porCliente')
+    @Post('porCliente')
     crearOrden(@Body() crearOrdenDto: CrearOrdenClienteDto,@User() jwt: JwtPayloadDto) {
-        return this.ordenesService.crearOrdenPorCliente(crearOrdenDto, jwt.sub);
+        this.ordenesService.crearOrdenPorCliente(crearOrdenDto, jwt.sub);
     }
 }
