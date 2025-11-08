6d70017d0d6b878c602c07c4e67006510025bfaa
Diego Becerra <d.becerraz@uniandes.edu.co>
2025-11-08 18:58:02 -0500

    Feature: Enhance Proveedor functionality with findByPais method and DTO updates

--- libs/domain/perfiles-dm/src/lib/interfaces/repositories/Iproveedor.repository.interface.ts
+++ libs/domain/perfiles-dm/src/lib/interfaces/repositories/Iproveedor.repository.interface.ts
@@ -3,4 +3,5 @@ import { Proveedor } from 'src/lib/entities/proveedor.entity';
 export interface IProveedorRepository {
   create(proveedor: Proveedor): Promise<Proveedor>;
   findById(id: string): Promise<Proveedor | null>;
+  findByPais(paisId: number): Promise<Proveedor[]>;
 }
