18ec2e49f9084f91468b24087867ad7d62d00325
Mateo Caceres Arcila <m.caceresa23@uniandes.edu.co>
2025-11-11 21:04:29 -0500

    feat: consulta historial de ventas

--- libs/domain/ordenes-dm/src/lib/interfaces/repository/IOrdenes.repository.interface.ts
+++ libs/domain/ordenes-dm/src/lib/interfaces/repository/IOrdenes.repository.interface.ts
@@ -2,6 +2,7 @@ import { Ubicacion } from "@medi-supply/core";
 import { BodegaOrigen, Orden, OrdenEntrega } from "src/lib/entities/orden.entity";
 import { Vehiculo } from "src/lib/entities/vehiculo.entity";
 import { RutaGenerada, RutaVehiculo } from "../vo/RepartoOrden.interface";
+import { HistorialVenta } from "../vo/historial-venta.interfaces";
 
 export interface IOrdenesRepository {
   crearOrden(orden: Orden): Promise<Orden>;
@@ -12,6 +13,10 @@ export interface IOrdenesRepository {
   guardarRutaDeReparto(vehiculoId:string, ruta:RutaGenerada): Promise<string>;
   buscarOrdenPorId(ordenId:string): Promise<Orden | null>;
   buscarRutaPorOrdenId(ordenId:string): Promise<RutaGenerada | null>;
+  obtenerHistorialVentasPorCliente(
+    vendedorId: string,
+    clienteId: string
+  ): Promise<HistorialVenta[]>;
 }
 
 export interface FiltrosEntrega {
